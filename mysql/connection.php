b05d3bf61257cf8bffa2457dbbd5cc2f08eedbbe
ibrahim gunes <72892355+ibrahimgunes0@users.noreply.github.com>
2023-07-14 14:19:45 +0300

    Add files via upload

---
+++ mysql/connection.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+session_start();
+
+include_once ('db.php');
+$mysqlServer = "localhost";
+$mysqlUseName = "root";
+$mysqlPassword = "";
+
+try {
+    $conn = new PDO("mysql:host=$mysqlServer;dbname=root;charset=utf8", $mysqlUseName, $mysqlPassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    db::$conn = $conn;
+}
+catch(PDOException $e)
+{
+    echo "Bağlantı hatası: " . $e->getMessage();
+}
